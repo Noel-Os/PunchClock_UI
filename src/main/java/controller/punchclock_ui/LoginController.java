2dd5d24adf933f8186fc3ec49cb970c508789447
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 20:52:15 +0200

    Initial Commit

--- src/main/java/controller/punchclock_ui/LoginController.java
+++ src/main/java/controller/punchclock_ui/LoginController.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LoginController {
 
     @FXML
