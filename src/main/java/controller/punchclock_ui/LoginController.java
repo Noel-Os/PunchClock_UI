4a95734b8f2870e1cc79ac33865885807fe250c5
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 22:35:21 +0200

    Post Request for login

--- src/main/java/controller/punchclock_ui/LoginController.java
+++ src/main/java/controller/punchclock_ui/LoginController.java
@@ -4,7 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginController {
 
@@ -14,14 +20,30 @@ public class LoginController {
     @FXML
     PasswordField password;
 
-    public void Login(ActionEvent actionEvent) {
+    public void Login(ActionEvent actionEvent) throws IOException {
 
         JSONObject jObject = new JSONObject();
 
         jObject.put("id", 0);
         jObject.put("password", password.getText());
-        jObject.put("role", null);
+        jObject.put("role", new JSONArray());
         jObject.put("username", username.getText());
+
+        URL url = new URL("http://localhost:8080/auth/login/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString = jObject.toString();
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        System.out.println(jsonInputString);
+
+        System.out.println(con.getResponseCode());
 
     }
 }
