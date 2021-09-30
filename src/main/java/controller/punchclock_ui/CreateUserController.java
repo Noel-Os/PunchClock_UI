8bc310a58d3539a1517cc3b2a242d0e8d7f54e59
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-30 08:57:24 +0200

    Create FXML Attribute username

--- src/main/java/controller/punchclock_ui/CreateUserController.java
+++ src/main/java/controller/punchclock_ui/CreateUserController.java
@@ -1,4 +1,11 @@
 package controller.punchclock_ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CreateUserController {
+
+    @FXML
+    TextField username;
+
 }
