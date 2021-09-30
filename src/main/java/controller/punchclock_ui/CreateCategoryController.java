cbc00c362228b4c31ec6bda200d69e37fdf42ce0
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-30 09:12:39 +0200

    Create FXML Attribute catName

--- src/main/java/controller/punchclock_ui/CreateCategoryController.java
+++ src/main/java/controller/punchclock_ui/CreateCategoryController.java
@@ -1,8 +1,13 @@
 package controller.punchclock_ui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class CreateCategoryController {
+
+    @FXML
+    TextField catName;
 
     public void create(ActionEvent actionEvent) {
     }
