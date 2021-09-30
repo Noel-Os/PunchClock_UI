136d01bc48cf701268ab16f341295b07764cb8ee
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-30 12:53:29 +0200

    Edit login-view.fxml

--- src/main/java/controller/punchclock_ui/LoginController.java
+++ src/main/java/controller/punchclock_ui/LoginController.java
@@ -4,10 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -23,29 +24,35 @@ public class LoginController {
     public void Login(ActionEvent actionEvent) throws IOException {
 
         JSONObject jObject = new JSONObject();
-
-        jObject.put("id", 0);
         jObject.put("password", password.getText());
-        jObject.put("role", new JSONArray());
         jObject.put("username", username.getText());
 
-        URL url = new URL("http://localhost:8080/auth/login/");
+        URL url = new URL("http://localhost:8080/auth/login");
+
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json; utf-8");
-        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Content-Type", "application/json");
+
         con.setDoOutput(true);
-        String jsonInputString = jObject.toString();
-        try (OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            os.write(input, 0, input.length);
+        OutputStream os = con.getOutputStream();
+        os.write(jObject.toString().getBytes());
+        os.flush();
+        os.close();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        con.getInputStream()));
+        String inputLine;
+
+        String tokenJSON = "";
+        while ((inputLine = in.readLine()) != null) {
+            tokenJSON = inputLine;
         }
+        in.close();
 
-        System.out.println(jsonInputString);
+        if (con.getResponseCode() == 200) {
+            Main.getInstance().toAdminHome();
+        } else {
 
-        System.out.println(con.getResponseCode());
-
-        Main.getInstance().toAdminHome();
-
+        }
     }
 }
