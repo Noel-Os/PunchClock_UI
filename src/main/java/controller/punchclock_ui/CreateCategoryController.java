85502811c01d2ee01a18926fb2b1e1f907b551b4
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-30 09:13:17 +0200

    Create Method back

--- src/main/java/controller/punchclock_ui/CreateCategoryController.java
+++ src/main/java/controller/punchclock_ui/CreateCategoryController.java
@@ -4,11 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class CreateCategoryController {
 
     @FXML
     TextField catName;
 
     public void create(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) throws IOException {
+        Main.getInstance().toAdminHome();
     }
 }
